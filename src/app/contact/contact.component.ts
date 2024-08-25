794e4d083d92057a7b878ca1860d7b8b0820b6ab
Ismael Vicente muñoz <ismael@mbp-de-ismael.lan>
2024-08-25 20:35:13 +0200

    hello its you

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+
 import { NavbarComponent } from '../navbar/navbar.component';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+
+  isLoaded: boolean = false;
+
+
+  ngOnInit() {
+    // Si la imagen ya está en caché, forzamos la animación
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 100);
+  }
+
+  onImageLoad() {
+    this.isLoaded = true;
+  }
 
 }
